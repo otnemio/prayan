2a78fc5dbc48e105f1cc7f7eb944ff21bbe0aeb5
Mayank Sachan <otnemio@gmail.com>
2024-02-29 18:31:32 +0530

    login using shoonya api

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
-import logging, time, grpc, priyu_pb2, priyu_pb2_grpc
+import os, yaml, logging, time, grpc, priyu_pb2, priyu_pb2_grpc
 from rich.logging import RichHandler
 import multiprocessing as mp
 from concurrent import futures
+from NorenRestApiPy.NorenApi import  NorenApi
 
 class Servicer(priyu_pb2_grpc.ChirperServicer):
     
@@ -10,13 +11,16 @@ class Servicer(priyu_pb2_grpc.ChirperServicer):
         return priyu_pb2.PReply(msg=f"Good")
 
 def initialize():
-    global log, jobs
+    global log, jobs, api
     jobs = []
     log = logging.getLogger("rich")
-
+    api = None
+    # abspath = os.path.abspath(__file__)
+    # dname = os.path.dirname(abspath)
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
     FORMAT = "%(message)s"
     logging.basicConfig(
-        level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+        level="INFO", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
     )
 
 def serve():
@@ -27,10 +31,61 @@ def serve():
     server.wait_for_termination()
     log.info("Server Started. Listening at [::]:50051")
 
-def shoonya(PIN):    
+def shoonya(TOTP):
+    api = ShoonyaApiPy()
+    api.fulllogin(TOTP)
     while True:
         log.info("Hello, World!")
-        time.sleep(5)
+        time.sleep(15)
+
+class ShoonyaApiPy(NorenApi):
+    
+    def __init__(self):
+        self.feed_opened = False
+        self.loggedin = False
+        self.list_tokens = []
+        NorenApi.__init__(self, host='https://api.shoonya.com/NorenWClientTP/', websocket='wss://api.shoonya.com/NorenWSTP/')        
+        # api = self
+    
+    def event_handler_feed_update(self,tick_data):
+        if self.feed_opened:
+            log.info(f"feed update {tick_data}")
+
+    def event_handler_order_update(self,tick_data):
+        if self.feed_opened:
+            log.info(f"order update {tick_data}")
+
+    def open_callback(self):
+        self.feed_opened = True
+
+    def close_callback(self):
+        self.feed_opened = False
+
+    def fulllogin(self,TOTP):
+        try:
+            with open("cred.yaml","r") as stream:
+                cred = yaml.safe_load(stream)
+            if not self.loggedin:
+                ret = self.login(cred['user'],cred['pwd'],TOTP,cred['vc'],cred['apikey'],cred['imei'])
+                if not ret:
+                    log.error("Problemia while trying to log in.")
+                elif ret['stat']=='Ok':
+                    self.loggedin = True
+                    self.start_websocket(order_update_callback=self.event_handler_order_update,
+                        subscribe_callback=self.event_handler_feed_update, 
+                        socket_open_callback=self.open_callback,
+                        socket_close_callback=self.close_callback)
+                    
+                    with open("instruments.yaml","r") as stream:
+                        instruments = yaml.safe_load(stream)
+                        for instrument in instruments:
+                            for key, value in instrument.items():
+                                for exch, token in value.items():
+                                    self.list_tokens.append(f"{exch}|{token}")
+                    self.subscribe(self.list_tokens,feed_type=2)
+                    log.info(f"Logged in: {self.loggedin}")
+        except Exception as e:
+                log.error(e)
 
 if __name__ == '__main__':
     initialize()
