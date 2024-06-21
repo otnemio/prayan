90c7b2e02f78e1b8e4963106ff9999498105beb3
Mayank Sachan <otnemio@gmail.com>
2024-06-21 18:36:28 +0530

    add listview

--- app_flutter/lib/src/pages/portfolio.dart
+++ app_flutter/lib/src/pages/portfolio.dart
@@ -1,112 +1,136 @@
 import 'package:flutter/material.dart';
-Widget mPortfolio(){
-return DefaultTabController(
-              length: 2,
-              child: Scaffold(
-                appBar: AppBar(
-                  title: const TabBar(
-                    tabs: <Widget>[
-                      Tab(
-                        text: 'Holdings',
-                      ),
-                      Tab(
-                        text: 'Positions',
-                      ),
-                    ],
+
+Widget mPortfolio() {
+  return DefaultTabController(
+    length: 2,
+    child: Scaffold(
+      appBar: AppBar(
+        title: const TabBar(
+          tabs: <Widget>[
+            Tab(
+              text: 'Holdings',
+            ),
+            Tab(
+              text: 'Positions',
+            ),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        children: <Widget>[
+          Center(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 120,
+                  child: Card(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 25),
+                          child: const Wrap(
+                            direction: Axis.vertical,
+                            children: [
+                              Text("Current:"),
+                              Text("10075.80"),
+                              Text("Invested:"),
+                              Text("9005.10"),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 25),
+                          child: Wrap(
+                            direction: Axis.vertical,
+                            children: [
+                              const Text("Pnl:"),
+                              priceText(1070.70),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-                body: TabBarView(
-                  children: <Widget>[
-                    Center(
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            height: 120,
-                            child: Card(
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Container(
-                                    margin: const EdgeInsets.symmetric(
-                                        horizontal: 25),
-                                    child: const Wrap(
-                                      direction: Axis.vertical,
-                                      children: [
-                                        Text("Current:"),
-                                        Text("10075.80"),
-                                        Text("Invested:"),
-                                        Text("9005.10"),
-                                      ],
-                                    ),
-                                  ),
-                                  Container(
-                                    margin: const EdgeInsets.symmetric(
-                                        horizontal: 25),
-                                    child: Wrap(
-                                      direction: Axis.vertical,
-                                      children: [
-                                        const Text("Pnl:"),
-                                        priceText(1070.70),
-                                      ],
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
+                Expanded(child: mListView()),
+              ],
+            ),
+          ),
+          Center(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 120,
+                  child: Card(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 25),
+                          child: Wrap(
+                            direction: Axis.vertical,
+                            children: [
+                              const Text("Total MTM:"),
+                              priceText(788.20),
+                            ],
                           ),
-                        ],
-                      ),
-                    ),
-                    Center(
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            height: 120,
-                            child: Card(
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Container(
-                                    margin: const EdgeInsets.symmetric(
-                                        horizontal: 25),
-                                    child: Wrap(
-                                      direction: Axis.vertical,
-                                      children: [
-                                        const Text("Total MTM:"),
-                                        priceText(788.20),
-                                      ],
-                                    ),
-                                  ),
-                                  Container(
-                                    margin: const EdgeInsets.symmetric(
-                                        horizontal: 25),
-                                    child: Wrap(
-                                      direction: Axis.vertical,
-                                      children: [
-                                        const Text("Day's MTM:"),
-                                        priceText(575.20),
-                                      ],
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 25),
+                          child: Wrap(
+                            direction: Axis.vertical,
+                            children: [
+                              const Text("Day's MTM:"),
+                              priceText(575.20),
+                            ],
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
-              ),
-            );
+                Expanded(child: mListView()),
+              ],
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
 }
 
+final List<String> entries = <String>['A', 'B', 'C'];
+final List<int> colorCodes = <int>[600, 500, 100];
+
+Widget mListView() {
+  return ListView(
+    padding: const EdgeInsets.all(8),
+    children: <Widget>[
+      Container(
+        height: 50,
+        color: Colors.amber[600],
+        child: const Center(child: Text('Entry A')),
+      ),
+      Container(
+        height: 50,
+        color: Colors.amber[500],
+        child: const Center(child: Text('Entry B')),
+      ),
+      Container(
+        height: 50,
+        color: Colors.amber[100],
+        child: const Center(child: Text('Entry C')),
+      ),
+    ],
+  );
+}
 
 Widget priceText(text) {
   return Text(
     text.toString(),
     style: TextStyle(
-      color: text<0 ? Colors.red : Colors.green,
+      color: text < 0 ? Colors.red : Colors.green,
       fontSize: 22,
     ),
   );
